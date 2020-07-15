04c719b497e73c3c37c8ea9bf94ecdcfb7a143d8
Nargiz Hus <nargizlhus@gmail.com>
2020-07-15 14:33:24 -0400

    Initial Commit

---
+++ api-analytics.py
@@ -0,0 +1,13 @@
+from googleapiclient.discovery import build
+
+api_key = 'encryptedkey'
+
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+request = youtube.channels().list(
+    part='statistics',
+    forUsername='schafer5'
+)
+
+response = request.execute()
+print(response)
